440e09c7c61e40268850af7cc4d3febab337c6d4
Liferoijrm <pedroextrer@gmail.com>
2025-09-10 14:14:37 -0300

    :sparkles:feat: 634521B Os Numeros de Saldanha

---
+++ Codeforces/634521B___Os_Numeros_de_Saldanha.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<int> num(4);
+
+    for(int i = 0; i < 4; i++){
+        cin >> num[i];
+    }
+
+    sort(num.begin(), num.end());
+
+    int a = num[3] - num[2];
+    int b = num[3] - num[1];
+    int c = num[3] - num[0];
+
+    cout << a << ' ' << b << ' ' << c << '\n';
+
+    return 0;
+}
