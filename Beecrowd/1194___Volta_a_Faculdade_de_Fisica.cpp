77d09278d20f98ebd3aa3f594343db41b512ee44
Liferoijrm <pedroextrer@gmail.com>
2025-10-29 15:41:53 -0300

    :sparkles:feat: 1194 Volta a Faculdade de Fisica

---
+++ Beecrowd/1194___Volta_a_Faculdade_de_Fisica.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll v, t;
+    while (cin >> v >> t) {
+        ll s = 2*v*t;
+        cout << s << '\n';
+    }
+}
