2de6872fcaf07210ad32ebc34da4348534c0058a
Liferoijrm <pedroextrer@gmail.com>
2026-01-06 21:31:06 -0300

    :wrench:fix: script for updating stats

--- scripts/generate_stats.sh
+++ scripts/generate_stats.sh
@@ -26,5 +26,31 @@ A_AT=$(angle $AT)
 A_HR=$(angle $HR)
 A_LC=$(angle $LC)
 
-export CF CSES BEE AT HR LC TOTAL
-export A_CF A_CSES A_BEE A_AT A_HR A_LC
+R_CF=-90
+R_CSES=$(echo "$A_CF - 90" | bc)
+R_BEE=$(echo "$A_CF + $A_CSES - 90" | bc)
+R_AT=$(echo "$A_CF + $A_CSES + $A_BEE - 90" | bc)
+R_HR=$(echo "$A_CF + $A_CSES + $A_BEE + $A_AT - 90" | bc)
+R_LC=$(echo "$A_CF + $A_CSES + $A_BEE + $A_AT + $A_HR - 90" | bc)
+
+sed \
+  -e "s/{{TOTAL}}/$TOTAL/g" \
+  -e "s/{{CF}}/$CF/g" \
+  -e "s/{{CSES}}/$CSES/g" \
+  -e "s/{{BEE}}/$BEE/g" \
+  -e "s/{{AT}}/$AT/g" \
+  -e "s/{{HR}}/$HR/g" \
+  -e "s/{{LC}}/$LC/g" \
+  -e "s/{{A_CF}}/$A_CF/g" \
+  -e "s/{{A_CSES}}/$A_CSES/g" \
+  -e "s/{{A_BEE}}/$A_BEE/g" \
+  -e "s/{{A_AT}}/$A_AT/g" \
+  -e "s/{{A_HR}}/$A_HR/g" \
+  -e "s/{{A_LC}}/$A_LC/g" \
+  -e "s/{{R_CF}}/$R_CF/g" \
+  -e "s/{{R_CSES}}/$R_CSES/g" \
+  -e "s/{{R_BEE}}/$R_BEE/g" \
+  -e "s/{{R_AT}}/$R_AT/g" \
+  -e "s/{{R_HR}}/$R_HR/g" \
+  -e "s/{{R_LC}}/$R_LC/g" \
+  stats/stats.svg.template > stats/stats.svg
