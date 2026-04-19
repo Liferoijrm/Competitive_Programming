1719a539e0ff832b112910f491a5fa060d14f8ce
Liferoijrm <pedroextrer@gmail.com>
2026-04-19 04:32:46 -0300

    :sparkles:feat: basic CP template

---
+++ Templates/marcinoni_template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll,ll>;
+using vl = vector<ll>;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define dbg(x) cerr << #x << " = " << (x) << endl;
+
+void solve(){
+
+    ll ans = 0;
+    vl v;
+    cout << ans << endl;
+}
+
+int main() { fastio
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
